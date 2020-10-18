8c36b253aacc3911906f212f67d70c6a0a66758a
rjjoaquin <46547515+rjjoaquin@users.noreply.github.com>
2020-10-18 20:35:29 +0800

    Update pdo-template.php

--- pdo-template.php
+++ pdo-template.php
@@ -10,9 +10,15 @@ $dsn = "mysql:host=". $host .";dbname=" . $dbname;
 //set pdo connection
 
 $pdo = new PDO($dsn, $user, $password);
+//set default
+$pdo -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
 
 $statement = $pdo -> query("Select * from posts");
 
 while ($row = $statement-> fetch(PDO::FETCH_ASSOC)) {
 echo $row['title'];
+}
+
+while ($row = $statement-> fetch(PDO::FETCH_OBJ)) {
+echo $row-> title;
 }
